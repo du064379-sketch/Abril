ae1757cb073bf8e11b357e0ba919c20be30681b3
Abril Sanchez Urquieta <du064379@mail.com>
2026-09-01 18:15:41 -0400

    joel

---
+++ clase_02_07_2026/ejemplo.js
@@ -0,0 +1,23 @@
+ alert("se conecto correctamente");
+ function sumar() {
+    alert("La función sumar funciona");
+    
+
+    let num1 = parseInt(document.getElementsByName("num1")[0].value);
+    let num2 = parseInt(document.getElementsByName("num2")[0].value);
+    let resultado = num1 + num2 ;
+    let mostrar = document.getElementById("resultado");
+    mostrar.innerHTML = "suma " + resultado;
+    let mostrar2 = document.getElementById("resultado2");
+    let matriz = "" ;
+    for (let i = 0; i < num1; i++) {
+        for (let j = 0; j < num2; j++) {
+            matriz += "* ";
+        }
+        matriz += "<br>";
+    }
+    mostrar2.innerHTML = matriz;
+ }
+ 
+
+
