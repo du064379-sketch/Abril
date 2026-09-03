d8c6f2c4ff74a7a1fe2d3f42183dceb0a89d5916
Abril Sanchez Urquieta <du064379@mail.com>
2026-09-03 16:56:31 -0400

    numeros

---
+++ clase_02_07_2026/8.clase-03-09-2026/ejemplo3.js
@@ -0,0 +1,25 @@
+// Comparaciones
+let a=5, b=10;
+console.log("a = ", a,"b= ", b);
+console.log("a==b: ", a == b);
+console.log("a != b: = ", a != b);
+console.log("a < b: = ", a < b);
+console.log("a > b: = ", a > b);
+console.log("a <= b: = ", a <= b);
+console.log("a >= b: = ", a >= b);
+
+let anno=prompt("Independencia de Santa Cruz");
+if(anno==1810)
+{     console.log("correcto")}
+else
+{     console.log("Incorrecto")
+}
+let numero = parseInt(prompt("Ingrese un número:"));
+if(numero % 2 == 0)
+{
+    console.log("El número es par");
+}
+else
+{
+    console.log("El número es impar");
+}
